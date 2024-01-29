4dc2ca21b03e572e4ecda533d515531d5c504a62
Keith Smith <157392500+smithk4454@users.noreply.github.com>
2024-01-29 16:34:12 +0000

    fixed issues

--- M2/M2T1_Smith_Keith.cpp
+++ M2/M2T1_Smith_Keith.cpp
@@ -7,12 +7,15 @@ Keith Smith
 */
 
 #include <iostream>
+#include <iomanip>
 #include <string>
 using namespace std;
 
 int main()
 {
     cout << "M2T1" << endl;
+    cout << "Thank you for dining with us" << endl;
+    cout << "----------------------------" << endl;
     // set up variables
     string meal = "Value Meal";
     // all in $, except the percent
@@ -22,11 +25,15 @@ int main()
     double total = 0;
 
     // do calculations
+    tax_amount = meal_price * tax_percent;
+    total = meal_price + tax_amount;
 
     // print the reciept
-    // TODO: fix decimals
+    // print this once to set the decimals to only two
+    cout << fixed << setprecision(2);
     cout << "Value Meal: "  << "\t$" << meal_price << endl;
     cout << "Tax: "   << "\t\t$" << tax_amount << endl;
+    cout << "----------------------------" << endl;
     cout << "Total:"  << "\t\t$" << total << endl;
 
     return 0;
