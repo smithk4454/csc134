52a633880f546baf7ea6a43d8d033df329a4b3fb
Keith Smith <157392500+smithk4454@users.noreply.github.com>
2024-02-05 16:14:37 +0000

    fixed lab output

--- M2/M2LAB1_Smith_Keith.cpp
+++ M2/M2LAB1_Smith_Keith.cpp
@@ -5,23 +5,38 @@ Smith Keith
 2/5/24
 */
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main() {
     // Take 1 - just a rectangle
     // Declare variables
-    double length, width;
-    double area;
+    double length, width, height;
+    double volume, cost, charge, profit;
+    const double COST_PER_CUBIC_FOOT = 0.23;
+    const double CHARGE_PER_CUBIC_FOOT = 0.5;
+    cout << setprecision(2) << fixed << showpoint;
+
     // Ask for the user input
     cout << "What is the length of the crate? ";
     cin >> length;
     cout << "What is the width of the crate? ";
     cin >> width;
+    cout << "What is the height of the crate? ";
+    cin >> height;
+
     // Do calculations
-    area = length * width;
+    volume = length * width * height;
+    cost = volume * COST_PER_CUBIC_FOOT;
+    charge = volume * CHARGE_PER_CUBIC_FOOT;
+    profit = charge - cost;
 
     // print the answer
-    cout << "The area is: " <<  area << endl;
+    cout << "The volume of the crate is ";
+    cout << volume << " cubic feet.\n";
+    cout << "Cost to build: $" << cost << endl;
+    cout << "Charge to customer: $" << charge << endl;
+    cout << "Profit: $" << profit << endl;
 
     return 0;
 }
