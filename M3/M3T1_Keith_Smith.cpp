d3a1c0a8e16bd76e9dbaeb380ddfbe79ed227406
Keith Smith <157392500+smithk4454@users.noreply.github.com>
2024-02-14 15:58:51 +0000

    added second rectangle

--- M3/M3T1_Keith_Smith.cpp
+++ M3/M3T1_Keith_Smith.cpp
@@ -8,11 +8,26 @@ using namespace std;
 
 int main() {
     // Declare variables
-
-    // Ask for length and idth of first rectangle
+    double width1, height1, width2, height2;
+    double area1, area2;
+    
+    // Ask for length and width of first rectangle
+    cout << "What is the width of the first rectangle? ";
+    cin >> width1;
+    cout << "What is the height of the first rectangle? ";
+    cin >> height1;
+    cout << "What is the width of the second rectangle? ";
+    cin >> width2;
+    cout << "What is the height of the second rectangle? ";
+    cin >> height2;
 
     // Calculate the areas
+    area1 = width1 * height1;
+    area2 = width2 * height2;
 
     // Print the areas
+    cout << "The area of the first rectangle is " << area1 << endl;
+    cout << "The area of the second rectangle is " << area2 << endl;
+    
         return 0;
 }
