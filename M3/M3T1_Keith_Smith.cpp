ca12e693acccb2c3c0119bd1eb1d6a4133f893e9
Keith Smith <157392500+smithk4454@users.noreply.github.com>
2024-02-14 15:36:49 +0000

    Made starter

---
+++ M3/M3T1_Keith_Smith.cpp
@@ -0,0 +1,11 @@
+// CSC 134 
+// M3T1 
+// Keith Smith
+// 02/14/24
+
+#include <iostream>
+using namespace std;
+
+int main() {
+        return 0;
+}
---
+++ Starter_Keith_Smith.cpp
@@ -0,0 +1,11 @@
+// CSC 134 
+// Assignment 
+// Keith Smith
+// Date
+
+#include <iostream>
+using namespace std;
+
+int main() {
+        return 0;
+}
