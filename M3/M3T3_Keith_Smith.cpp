1406081ea2dcbdaf593c9e7dd6ce778dd2f813da
Keith Smith <157392500+smithk4454@users.noreply.github.com>
2024-02-21 16:01:43 +0000

    roll two die

--- M3/M3T3_Keith_Smith.cpp
+++ M3/M3T3_Keith_Smith.cpp
@@ -4,30 +4,33 @@
 // 21/02/24
 
 #include <iostream>
+#include <cstdlib> // for rand()
 using namespace std;
 
 int main() {
     // Declare variables
     int roll;
+    int die1, die2;
 
     // Ask
-    cout << "Welcome to the craps table" << endl;
-    cout << "What did you roll? ";
-    cin >> roll;
+     cout << "Welcome to the craps table" << endl;
+    
+    die1 = (rand() % 6) +1;
+    die2 = (rand() % 6) +1;
+    roll = die1 + die2;
 
     // Calculate
     cout << "Your roll was : " << roll << endl;
-    if (roll == 7) {
+    if (roll == 7 || roll == 11) {
         cout << "You Win!" << endl;
     }
-    else if (roll == 2) {
+    else if (roll == 2 || roll 3 || roll == 12) {
         cout << "You lose..." << endl;
     }
     else {
         cout << "Your point is: " << roll << endl;
         cout << "TODO: roll the point" << endl;
     }
-    // Print
 
         return 0;
 }
