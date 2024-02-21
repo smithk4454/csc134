0905f9ff956fd070697884b06c500479a4d985df
Keith Smith <157392500+smithk4454@users.noreply.github.com>
2024-02-21 15:48:31 +0000

    added dice roll

--- M3/M3T3_Keith_Smith.cpp
+++ M3/M3T3_Keith_Smith.cpp
@@ -8,13 +8,25 @@ using namespace std;
 
 int main() {
     // Declare variables
+    int roll;
+
+    // Ask
     cout << "Welcome to the craps table" << endl;
     cout << "What did you roll? ";
     cin >> roll;
-    // Ask
 
     // Calculate
-
+    cout << "Your roll was : " << roll << endl;
+    if (roll == 7) {
+        cout << "You Win!" << endl;
+    }
+    else if (roll == 2) {
+        cout << "You lose..." << endl;
+    }
+    else {
+        cout << "Your point is: " << roll << endl;
+        cout << "TODO: roll the point" << endl;
+    }
     // Print
 
         return 0;
