6881e8d4bc4b6b186969a104d78825e83413238a
Keith Smith <157392500+smithk4454@users.noreply.github.com>
2024-02-21 15:41:47 +0000

    Started M3T3

---
+++ M3/M3T3_Keith_Smith.cpp
@@ -0,0 +1,21 @@
+// CSC 134 
+// M3T3
+// Keith Smith
+// 21/02/24
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Declare variables
+    cout << "Welcome to the craps table" << endl;
+    cout << "What did you roll? ";
+    cin >> roll;
+    // Ask
+
+    // Calculate
+
+    // Print
+
+        return 0;
+}
