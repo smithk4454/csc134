22b83a0340c698b2f989c5d0455fd56f25809d14
Keith Smith <157392500+smithk4454@users.noreply.github.com>
2024-04-10 14:42:48 +0000

    started

---
+++ M6LAB_Smith_Keith/M6LAB_Smith_Keith.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+/*
+CSC 134
+M6LAB1 - Buckshot Roulette
+Smith Keith
+4/10
+Array representing a magazine containing live and dummy rounds
+*/
+
+// constant variables
+const int MAX_SIZE = 6; // never more than six shells, size of array
+const int EMPTY = 0;
+const int BLANK = 1;
+const int LIVE  = 2;
+
+// global array for the magazine
+int magazine[MAX_SIZE] magazine = {0};
+
+int main() {
+    cout << "M6LAB1" << endl;
+    // range based for loop
+    for (int shell : magazine) {
+        cout << shell << endl;
+    }
+    return 0;
+}
