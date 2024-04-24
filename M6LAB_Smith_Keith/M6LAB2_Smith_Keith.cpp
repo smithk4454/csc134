c6682f6a41d4a0115d1237b21c87bf0135d43303
Keith Smith <157392500+smithk4454@users.noreply.github.com>
2024-04-24 10:26:15 -0400

    Update M6LAB2_Smith_Keith.cpp

--- M6LAB_Smith_Keith/M6LAB2_Smith_Keith.cpp
+++ M6LAB_Smith_Keith/M6LAB2_Smith_Keith.cpp
@@ -9,7 +9,28 @@
 
 using namespace std;
 
+// 3 examples
+void array_example();
+void vector_example();
+void linked_list();
+
 int main() {
   cout << "M6LAB2" << endl;
+  array_example();
+  vector_example();
+  linked_list();
   return 0;
+}
+
+// 3 examples -- full functions
+void array_example() {
+    cout << "Example 1: Array" << endl;
+    const int SIZE = 5; // absolute max
+    int max = 3;        // current max
+    int nums[SIZE] = {1, 2, 2, 0, 0};
+    // print each item
+    for (int i=0; i<max; i++) {
+      cout << nums[i] << ",";
+    }
+    cout << endl;
 }
